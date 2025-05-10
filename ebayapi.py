49194a17f6c4ddbd46f70770ba16061be6250154
HaritsAcheiz <harits.muhammad.only@gmail.com>
2025-05-10 07:53:40 +0700

    add oauth feature

--- ebayapi.py
+++ ebayapi.py
@@ -3,6 +3,9 @@ from dataclasses import dataclass
 from urllib.parse import urljoin
 from os import getenv
 from dotenv import load_dotenv
+import ebayoauth
+from ebayoauth.oauthclient.oauth2api import oauth2api
+from ebayoauth.oauthclient.model.model import environment
 
 load_dotenv()
 
@@ -10,6 +13,13 @@ load_dotenv()
 @dataclass
 class EbayAPI():
     api_base_url: str = 'https://api.ebay.com'
+
+    def access_token(self):
+        oauthapi = oauth2api()
+        scopes = ['https://api.ebay.com/oauth/api_scope']
+        auth_url = oauthapi.generate_user_authorization_url(environment.Production, scopes)
+
+        return auth_url
 
     def search(self, q, limit=25, offset=0):
         endpoint = urljoin(self.api_base_url, '/buy/browse/v1/item_summary/search')
@@ -27,7 +37,7 @@ class EbayAPI():
             response = client.get(endpoint, params=params)
             return response
 
-    def get_product(self, product_id):
+    def product(self, product_id):
         endpoint = urljoin(self.api_base_url, f'/buy/browse/v1/item/{product_id}')
         headers = {
             'Authorization': f"Bearer {getenv('EBAY_OAUTH_TOKEN')}",
@@ -41,8 +51,9 @@ class EbayAPI():
 
 if __name__ == "__main__":
     eapi = EbayAPI()
+    response = eapi.access_token()
     # response = eapi.search('drone', 3)
-    response = eapi.get_product('v1%7C145450205843%7C0')
+    # response = eapi.product('v1%7C145450205843%7C0')
 
     print(response)
-    print(response.json())
+    # print(response.json())
--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from utils import *
 
 if __name__ == '__main__':
     ebayapi = EbayAPI()
-    search_response = ebayapi.search('gas scooters')
-    search_df = parse_search_response(search_response.json())
+    # search_response = ebayapi.search('gas scooters')
+    # search_df = parse_search_response(search_response.json())
+
     search_df.to_csv('data/search_result.csv')
+
+    product_ids = search_df['itemId'].to_list()
+    product_detail_records = []
+    for product_id in product_ids:
+        response = ebayapi.product(product_id)
+
